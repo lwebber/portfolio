dff1fbd0162b15373d2edccece46b7cee13599aa
Laura Webber <wwebby1@gmail.com>
2019-10-22 22:46:09 -0700

    add navbar

---
+++ index.js
@@ -0,0 +1,16 @@
+function run() {
+    //add responsive class - translate to JQuery
+
+    respond();
+
+    function respond() {
+        let x = $("myTopnav");
+        if (x.hasClass("topnav")) {
+            x.addClass("responsive");
+        } else {
+            x.addClass("topnav");
+        }
+    }
+}
+
+$(run);
